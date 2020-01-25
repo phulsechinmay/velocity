bf8135fc4cd52cc9bf2a89f2e910fc2b1a710af8
nathanjbrockway <ninjabrockway@gmail.com>
2020-01-25 16:53:05 -0600

    added renderer to frontend

--- mapsapi.py
+++ mapsapi.py
@@ -1,12 +1,28 @@
-from flask import Flask
+#from flask import Flask
 import requests
 import json
 
-app = Flask(__name__)
+#app = Flask(__name__)
 auth = "AIzaSyCdqos0PqD7ozbyWxR-kLZwDSix4rhIoEM"
 
-def getDriections(origins, destination, waypoints):
+def getDriections(origin, destination, waypoints):
     url = "https://maps.googleapis.com/maps/api/directions/json?"
+    headers = {
+        "units": "imperial",
+        "mode": "walking",
+        "origin": origin,
+        "destination": destination,
+        "waypoints" : waypoints,
+        "key": auth
+    }
+    res = requests.get(url,params=headers)
+    if(res.status_code == 200):
+        res = res.json()
+        data = res["routes"]
+        print(data)
+        return res
+    else:
+        print("DIRECTIONS API ERROR")
 
 def getTravelTime(origin,destination,waypoints):
     url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
@@ -21,11 +37,13 @@ def getTravelTime(origin,destination,waypoints):
     res = requests.get(url,params=headers)
     if(res.status_code == 200):
         res = res.json()
-        duration = res["rows"][0]["elements"][0]["duration"]["text"]
-        print("Travel Time: " + str(duration))
-        return res
+        data = res["rows"][0]["elements"][0]
+        duration = data["duration"]["value"]
+        distance = data["distance"]["text"]
+        print("Distance: "+distance+", Travel Time: " + str(duration//60)+" mins")
+        return (distance,duration)
     else:
         print("DISTANCE MATRIX API ERROR")
 
 if __name__ == "__main__":
-    getTravelTime("2 Commons, Mosher Residence Hall, 725 Mosher Cir, College Station, TX 77840","Kyle Field","Sbisa Dining Hall, 233 Houston St, College Station, TX 77840")
+    getDriections("2 Commons, Mosher Residence Hall, 725 Mosher Cir, College Station, TX 77840","Kyle Field","Sbisa Dining Hall, 233 Houston St, College Station, TX 77840")

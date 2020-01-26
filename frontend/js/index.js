882ac246f5c8345f7c09099e4dbd9354ee660e6c
Josiah Coad <josiahcoad@client-10-229-176-229.tamulink.tamu.edu>
2020-01-26 09:23:51 -0600

    semicolons boyyyyy

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,13 +1,13 @@
 // Initialize and add the map
-var showTrafficLayer = false
-var showBikingLayer = false
-var showDirectionPanel = true
-var displayRoute
-var bikeRoute
-var carRoute
-var walkLeg
-var bikeLeg
-var currentRoute
+var showTrafficLayer = false;
+var showBikingLayer = false;
+var showDirectionPanel = true;
+var displayRoute;
+var bikeRoute;
+var carRoute;
+var walkLeg;
+var bikeLeg;
+var currentRoute;
 
 function initMap() {
   const positions = [
@@ -39,30 +39,30 @@ function initMap() {
     [30.6051064, -96.346981],
     [30.6050603, -96.3491375],
     [30.6013654, -96.3543799],
-  ]
+  ];
 
   var iconBase =
-    'https://developers.google.com/maps/documentation/javascript/examples/full/images/'
-  var directionsService = new google.maps.DirectionsService()
-  var directionsRenderer = new google.maps.DirectionsRenderer()
+    'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+  var directionsService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer();
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: {
       lat: positions[13][0],
       lng: positions[13][1],
     },
-  })
-  directionsRenderer.setMap(map)
+  });
+  directionsRenderer.setMap(map);
 
   var defaultBounds = new google.maps.LatLngBounds(
     new google.maps.LatLng(30.5159362, -96.5171539),
     new google.maps.LatLng(30.7215865, -96.2252309)
-  )
+  );
   var onChangeHandler = function() {
     if (($('#start').val() !== '') & ($('#end').val() !== '')) {
-      calculateAndDisplayRoute(directionsService, directionsRenderer)
+      calculateAndDisplayRoute(directionsService, directionsRenderer);
     }
-  }
+  };
 
   const startAutocomplete = new google.maps.places.Autocomplete(
     document.getElementById('start'),
@@ -70,8 +70,8 @@ function initMap() {
       bounds: defaultBounds,
       strictBounds: true,
     }
-  )
-  startAutocomplete.addListener('place_changed', onChangeHandler)
+  );
+  startAutocomplete.addListener('place_changed', onChangeHandler);
 
   const endAutocomplete = new google.maps.places.Autocomplete(
     document.getElementById('end'),
@@ -79,12 +79,12 @@ function initMap() {
       bounds: defaultBounds,
       strictBounds: true,
     }
-  )
-  endAutocomplete.addListener('place_changed', onChangeHandler)
+  );
+  endAutocomplete.addListener('place_changed', onChangeHandler);
 
   function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-    var start = document.getElementById('start').value
-    var end = document.getElementById('end').value
+    var start = document.getElementById('start').value;
+    var end = document.getElementById('end').value;
     directionsService.route(
       {
         origin: start,
@@ -93,8 +93,8 @@ function initMap() {
       },
       function(response, status) {
         if (status === 'OK') {
-          bikeRoute = response
-          bikeRoute.routes[0].warnings.pop()
+          bikeRoute = response;
+          bikeRoute.routes[0].warnings.pop();
           //directionsRenderer.setDirections(response);
           directionsService.route(
             {
@@ -104,37 +104,32 @@ function initMap() {
             },
             function(response, status) {
               if (status === 'OK') {
-                carRoute = response
-                directionsRenderer.setDirections(response)
-                displayBikeSuggestion()
+                carRoute = response;
+                directionsRenderer.setDirections(response);
+                displayBikeSuggestion();
               } else {
-                window.alert('Directions request failed due to ' + status)
+                window.alert('Directions request failed due to ' + status);
               }
             }
-          )
+          );
         } else {
-          window.alert('Directions request failed due to ' + status)
+          window.alert('Directions request failed due to ' + status);
         }
       }
-    )
+    );
   }
 
   function displayBikeSuggestion() {
-    var bikeTime = bikeRoute.routes[0].legs[0].duration
-    var carTime = carRoute.routes[0].legs[0].duration
-    $('#bikeMsg').text(
-      'Bike route is: ' +
-        bikeTime['text'])
-    $("#currentMsg").text(
-        'Current route is: ' +
-        carTime['text']
-    )
-    $("#bike-better-popup").show();
+    var bikeTime = bikeRoute.routes[0].legs[0].duration;
+    var carTime = carRoute.routes[0].legs[0].duration;
+    $('#bikeMsg').text('Bike route is: ' + bikeTime['text']);
+    $('#currentMsg').text('Current route is: ' + carTime['text']);
+    $('#bike-better-popup').show();
   }
 
   function route(origin, destination, travelMode) {
     if (!['WALKING', 'BIKING', 'DRIVING'].includes(travelMode)) {
-      alert('Unknown travel mode')
+      alert('Unknown travel mode');
     }
     directionsService.route(
       {
@@ -144,13 +139,13 @@ function initMap() {
       },
       function(response, status) {
         if (status === 'OK') {
-          currentRoute = response
-          directionsRenderer.setDirections(currentRoute)
+          currentRoute = response;
+          directionsRenderer.setDirections(currentRoute);
         } else {
-          window.alert('Directions request failed due to ' + status)
+          window.alert('Directions request failed due to ' + status);
         }
       }
-    )
+    );
   }
 
   function walkThenBike(origin, bike, destination) {
@@ -162,7 +157,7 @@ function initMap() {
       },
       function(response, status) {
         if (status === 'OK') {
-          walkLeg = response
+          walkLeg = response;
           //directionsRenderer.setDirections(response);
           directionsService.route(
             {
@@ -172,21 +167,21 @@ function initMap() {
             },
             function(response, status) {
               if (status === 'OK') {
-                bikeLeg = response
+                bikeLeg = response;
                 //directionsRenderer.setDirections(response);
-                walkLeg.routes[0].legs.push(bikeLeg.routes[0].legs[0])
-                walkLeg.routes[0].warnings.pop()
-                directionsRenderer.setDirections(walkLeg)
+                walkLeg.routes[0].legs.push(bikeLeg.routes[0].legs[0]);
+                walkLeg.routes[0].warnings.pop();
+                directionsRenderer.setDirections(walkLeg);
               } else {
-                window.alert('Directions request failed due to ' + status)
+                window.alert('Directions request failed due to ' + status);
               }
             }
-          )
+          );
         } else {
-          window.alert('Directions request failed due to ' + status)
+          window.alert('Directions request failed due to ' + status);
         }
       }
-    )
+    );
   }
 
   var parking = {
@@ -194,16 +189,16 @@ function initMap() {
     scaledSize: new google.maps.Size(30, 30),
     origin: new google.maps.Point(0, 0),
     anchor: new google.maps.Point(0, 0),
-  }
+  };
 
   var bike = {
     url: 'cycling.png',
     scaledSize: new google.maps.Size(30, 30),
     origin: new google.maps.Point(0, 0),
     anchor: new google.maps.Point(0, 0),
-  }
+  };
 
-  var stationMarkers = []
+  var stationMarkers = [];
 
   positions.forEach(function(position) {
     stationMarkers.push(
@@ -214,37 +209,37 @@ function initMap() {
         zindex: 10,
         visible: false,
       })
-    )
-  })
+    );
+  });
 
   google.maps.event.addListener(map, 'zoom_changed', function() {
-    var zoom = map.getZoom()
+    var zoom = map.getZoom();
     for (i = 0; i < stationMarkers.length; i++) {
-      stationMarkers[i].setVisible(zoom >= 15)
+      stationMarkers[i].setVisible(zoom >= 15);
     }
-  })
+  });
   // Function that gets called if you click bike marker
   const chooseBike = bikeMarker => {
-    const pos = bikeMarker.getPosition()
+    const pos = bikeMarker.getPosition();
     const start = $('#start').val();
     const end = $('#end').val();
     if (start & end) {
-      walkThenBike(start, pos.lat() + ',' + pos.lng(), end)
+      walkThenBike(start, pos.lat() + ',' + pos.lng(), end);
     } else if (start) {
-      route(start, pos.lat() + ',' + pos.lng(), 'WALKING')
+      route(start, pos.lat() + ',' + pos.lng(), 'WALKING');
     } else {
-      alert("Please enter a start location");
+      alert('Please enter a start location');
     }
-  }
+  };
   // Show bikes on map
   const showBikeMarkers = data => {
-    const bikeMarkers = []
+    const bikeMarkers = [];
     for (var i = 1; i < data.length; i++) {
-      const bike_data = data[i]
-      const lock_open = bike_data.lockStatus
+      const bike_data = data[i];
+      const lock_open = bike_data.lockStatus;
       if (lock_open) {
-        const lat = bike_data.location.lat
-        const lng = bike_data.location.lng
+        const lat = bike_data.location.lat;
+        const lng = bike_data.location.lng;
         const bikeMarker = new google.maps.Marker({
           position: {
             lat,
@@ -252,9 +247,9 @@ function initMap() {
           },
           map: map,
           icon: bike,
-        })
-        bikeMarker.addListener('click', () => chooseBike(bikeMarker))
-        bikeMarkers.push(bikeMarker)
+        });
+        bikeMarker.addListener('click', () => chooseBike(bikeMarker));
+        bikeMarkers.push(bikeMarker);
       }
     }
     new MarkerClusterer(map, bikeMarkers, {
@@ -262,66 +257,66 @@ function initMap() {
         'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
       gridSize: 30,
       minimumClusterSize: 10,
-    })
-  }
+    });
+  };
   $('#showBikeRoute').click(() => {
-    directionsRenderer.setDirections(bikeRoute)
-    $("#bike-better-popup").hide();
-  })
+    directionsRenderer.setDirections(bikeRoute);
+    $('#bike-better-popup').hide();
+  });
 
   $('#showDirections').click(() => {
-    showDirectionPanel = !showDirectionPanel
+    showDirectionPanel = !showDirectionPanel;
     directionsRenderer.setPanel(
       showDirectionPanel ? document.getElementById('right-panel') : null
-    )
-  })
+    );
+  });
   directionsRenderer.setPanel(
     showDirectionPanel ? document.getElementById('right-panel') : null
-  )
+  );
   // Show traffic
-  var trafficLayer = new google.maps.TrafficLayer()
+  var trafficLayer = new google.maps.TrafficLayer();
   $('#traffic-layer-checkbox').click(() => {
-    showTrafficLayer = !showTrafficLayer
-    trafficLayer.setMap(showTrafficLayer ? map : null)
-  })
+    showTrafficLayer = !showTrafficLayer;
+    trafficLayer.setMap(showTrafficLayer ? map : null);
+  });
   // Show bike layer
-  var bikeLayer = new google.maps.BicyclingLayer()
+  var bikeLayer = new google.maps.BicyclingLayer();
   $('#bike-path-checkbox').click(() => {
-    showBikingLayer = !showBikingLayer
+    showBikingLayer = !showBikingLayer;
     if (showBikingLayer) {
       $('#bikeLayerInfo').text(
         'Dark green routes indicated dedicated bicycle routes. Light green routes indicate streets with dedicated "bike lanes." Dashed routes indicate streets or paths otherwise recommended for bicycle usage.'
-      )
-      bikeLayer.setMap(map)
+      );
+      bikeLayer.setMap(map);
     } else {
-      $('#bikeLayerInfo').text(' ')
-      bikeLayer.setMap(null)
+      $('#bikeLayerInfo').text(' ');
+      bikeLayer.setMap(null);
     }
-  })
+  });
   // Show current location
-  var im = 'http://www.robotwoods.com/dev/misc/bluecircle.png'
+  var im = 'http://www.robotwoods.com/dev/misc/bluecircle.png';
   navigator.geolocation.getCurrentPosition(
     position => {
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
-      }
-      var myLatLng = new google.maps.LatLng(pos.lat, pos.lng)
+      };
+      var myLatLng = new google.maps.LatLng(pos.lat, pos.lng);
       new google.maps.Marker({
         position: myLatLng,
         map: map,
         icon: im,
-      })
-      map.setCenter(pos)
+      });
+      map.setCenter(pos);
       // Get nearby bikes
       getNearbyBikes(pos.lat, pos.lng)
         .then(showBikeMarkers)
-        .catch(alert)
+        .catch(alert);
       // Fill current location in start box
-      $('#start').val(pos.lat.toFixed(5) + ',' + pos.lng.toFixed(5))
+      $('#start').val(pos.lat.toFixed(5) + ',' + pos.lng.toFixed(5));
     },
     () => alert('navigator.geolocation failed, may not be supported')
-  )
+  );
   // Clear start input box if user places cursor into box
-  $('#start').click(() => $('#start').val(''))
+  $('#start').click(() => $('#start').val(''));
 }
